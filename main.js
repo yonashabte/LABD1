b967bad71a914ca5d190c0ded195d71fa55903f6
yonashabte <yonaschereta@gmail.com>
2019-02-21 20:54:36 -0500

    created html css js

---
+++ main.js
@@ -0,0 +1,6 @@
+let yourName = prompt('what is your Name?')
+let favoriteMovie = prompt('what is your favorite Movie?')
+let favoritefood = prompt('what is your favorite food?')
+confirm(`${Yonas} your favorite movie is ${x-men} and you love ${kitfo}`)
+
+console.log(`${Yonas} your favorite movie is ${x-men} and you love ${kitfo}`)
